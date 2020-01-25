cf22d7a82c45c91c6756831b4740621fbee52ab4
Anna Terehova <anna.terehova@gmail.com>
2020-01-25 19:16:30 +0200

    1. New fields added to the Product class: - discount - description - category 2. price, name and discount validation added; 3. Minor changes in the user menu to remove the bug found.

--- src/com/javaguru/shoppinglist/Product.java
+++ src/com/javaguru/shoppinglist/Product.java
@@ -7,6 +7,10 @@ public class Product {
     private Long id;
     private String name;
     private BigDecimal price;
+    private String category;
+    private String description;
+    private BigDecimal discount;
+
 
     public Long getId() {
         return id;
@@ -20,16 +24,52 @@ public class Product {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setName(String name) throws Exception {
+        if (name.length() >= 3 && name.length() <= 32) {
+            this.name = name;
+        } else {
+            throw new Exception("The name cannot be shorter than 3 symbols or longer than 32 symbols");
+        }
     }
 
     public BigDecimal getPrice() {
         return price;
     }
 
-    public void setPrice(BigDecimal price) {
-        this.price = price;
+    public void setPrice(BigDecimal price) throws Exception {
+        if (price.signum() == 1) {
+            this.price = price;
+        } else {
+            throw new Exception("Price must be nonzero positive");
+        }
     }
 
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(BigDecimal discount) throws Exception {
+        if (discount.compareTo(price) <= 0) {
+            this.discount = discount;
+        } else {
+            throw new Exception("The discount cannot exceed the price");
+        }
+
+    }
 }
--- src/com/javaguru/shoppinglist/ShoppingListApplication.java
+++ src/com/javaguru/shoppinglist/ShoppingListApplication.java
@@ -21,25 +21,37 @@ class ShoppingListApplication {
                     case 1:
                         System.out.println("Enter product name: ");
                         String name = scanner.nextLine();
+                        System.out.println("Enter product description: ");
+                        String description = scanner.nextLine();
+                        System.out.println("Enter product category: ");
+                        String category = scanner.nextLine();
                         System.out.println("Enter product price: ");
                         BigDecimal price = new BigDecimal(scanner.nextLine());
+                        System.out.println("Enter discount: ");
+                        BigDecimal discount = new BigDecimal(scanner.nextLine());
                         Product product = new Product();
                         product.setName(name);
+                        product.setDescription(description);
+                        product.setCategory(category);
                         product.setPrice(price);
+                        product.setDiscount(discount);
                         product.setId(productIdSequence);
                         productRepository.put(productIdSequence, product);
                         productIdSequence++;
                         System.out.println("Result: " + product.getId());
+                        break;
                     case 2:
                         System.out.println("Enter product id: ");
                         long id = scanner.nextLong();
                         Product findProductResult = productRepository.get(id);
                         System.out.println(findProductResult);
+                        break;
                     case 3:
                         return;
                 }
             } catch (Exception e) {
                 System.out.println("Error! Please try again.");
+                System.out.println(e.getMessage());
             }
         }
     }
