17afb2615bcaef653c48626b54f9245e882294f1
Anna Terehova <anna.terehova@gmail.com>
2020-05-01 11:13:54 +0300

    Dependency Injection implemented using Spring annotations and Java code configuration.

--- src/main/java/shoppinglist/com/javaguru/config/AppConfig.java
+++ src/main/java/shoppinglist/com/javaguru/config/AppConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = "shoppinglist.com.javaguru")
+@ComponentScan("shoppinglist.com.javaguru")
 public class AppConfig {
 
 }
